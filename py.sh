79073ddd847c9d2259d92299d9c690cdde3f278b
hthuongw1112 <hthuongw1112@gmail.com>
2025-03-09 12:02:43 +0700

    Create py.sh

---
+++ py.sh
@@ -0,0 +1,10 @@
+pip install --upgrade pip && \
+pip install poetry invoke pre-commit pipx jupyterlab ipython \
+    numpy scipy pandas sympy matplotlib seaborn statsmodels \
+    tensorflow torch torchvision torchaudio jax flax optax \
+    fastapi pydantic starlette httpx uvicorn gunicorn \
+    flask django celery redis pymongo psycopg2-binary mysqlclient \
+    cryptography paramiko fabric boto3 pyjwt pycryptodome \
+    scapy mitmproxy beautifulsoup4 lxml selenium \
+    pynvim neovim autopep8 pylint mypy black isort \
+    opencv-python-headless imageio pillow
